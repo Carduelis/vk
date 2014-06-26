f16128b1cace46652fb18b2b3c297d0d3ae2bc84
Alexander Toropov <torf993@gmail.com>
2014-06-26 11:40:29 +0400

    Создание интерфейса дял настройки целей

--- targetssettings.cpp
+++ targetssettings.cpp
@@ -1,10 +1,25 @@
 #include"targetssettings.h"
 #include"ui_targetssettings.h"
+#include<QDebug>
 
 TargetsSettings::TargetsSettings(QWidget *parent) : QMainWindow(parent),ui(new Ui::TargetsSettings)
 {
     ui->setupUi(this);
-    //vui->Targets
+    targets_count=ui->Targets->count();
+    QCheckBox *checkbox;
+    for(quint8 widget=0u;widget<targets_count;widget++)
+    {
+        ui->Targets->setCurrentIndex(widget);
+        checkbox=ui->Targets->currentWidget()->findChild<QCheckBox*>(QString("%1%2").arg("TargetGoHome").arg(widget+1)),
+        connect(
+            checkbox,
+            SIGNAL(stateChanged(int)),
+            this,
+            SLOT(on_TargetsGoHome_stateChanged(int))
+        );
+        checkbox->stateChanged(checkbox->checkState());
+    }
+    ui->Targets->setCurrentIndex(0);
 }
 
 TargetsSettings::~TargetsSettings()
@@ -12,12 +27,46 @@ TargetsSettings::~TargetsSettings()
     delete ui;
 }
 
+quint8 TargetsSettings::GetTargetsGount()
+{
+    return targets_count;
+}
+
 void TargetsSettings::on_ApplyTargets_clicked()
 {
 
 }
 
-void TargetsSettings::on_TargetGoHome1_clicked()
+void TargetsSettings::on_TargetsGoHome_stateChanged(int arg)
 {
+    QRadioButton *radio1,*radio2;
+    radio1=QObject::sender()->parent()->findChild<QRadioButton*>(QString("%1%2").arg("TargetGoHome150").arg(ui->Targets->currentIndex()+1));
+    radio2=QObject::sender()->parent()->findChild<QRadioButton*>(QString("%1%2").arg("TargetGoHome330").arg(ui->Targets->currentIndex()+1));
+    if((radio1->isChecked() && radio2->isChecked()) || (!radio1->isChecked() && !radio2->isChecked()))
+    {
+        radio1->setChecked(true);
+        radio2->setChecked(false);
+    }
+    radio1->setEnabled(arg==2);
+    radio2->setEnabled(arg==2);
+}
 
+void TargetsSettings::on_SwitchTargetNext_clicked()
+{
+    quint8 pos=static_cast<quint8>(ui->Targets->currentIndex());
+    if(pos<GetTargetsGount()-1u)
+        pos++;
+    else
+        pos=0u;
+    ui->Targets->setCurrentIndex(pos);
+}
+
+void TargetsSettings::on_SwitchTargetPrev_clicked()
+{
+    quint8 pos=static_cast<quint8>(ui->Targets->currentIndex());
+    if(pos>0u)
+        pos--;
+    else
+        pos=GetTargetsGount()-1u;
+    ui->Targets->setCurrentIndex(pos);
 }
--- targetssettings.h
+++ targetssettings.h
@@ -1,27 +1,33 @@
 #ifndef TARGETSSETTINGS_H
 #define TARGETSSETTINGS_H
 
-#include <QMainWindow>
+#include<QMainWindow>
 
-namespace Ui {
-class TargetsSettings;
+namespace Ui
+{
+    class TargetsSettings;
 }
 
 class TargetsSettings : public QMainWindow
 {
     Q_OBJECT
 
-public:
-    explicit TargetsSettings(QWidget *parent = 0);
-    ~TargetsSettings();
+    public:
+        explicit TargetsSettings(QWidget *parent = 0);
+        ~TargetsSettings();
+        quint8 GetTargetsGount();
 
-private slots:
-    void on_ApplyTargets_clicked();
+    private slots:
+        void on_ApplyTargets_clicked();
+        void on_TargetsGoHome_stateChanged(int arg);
 
-    void on_TargetGoHome1_clicked();
+        void on_SwitchTargetNext_clicked();
+
+        void on_SwitchTargetPrev_clicked();
 
 private:
-    Ui::TargetsSettings *ui;
+        quint8 targets_count;
+        Ui::TargetsSettings *ui;
 };
 
 #endif // TARGETSSETTINGS_H
