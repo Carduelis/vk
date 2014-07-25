28887aeec4adcebd9c6524d838ae3e417aa6dd86
K3lwiN <k3lwin21@gmail.com>
2014-07-25 16:15:58 +0400

    Переписаны два упражнения ДКРМ

--- js/data_prmg/dkrm/algorithms.js
+++ js/data_prmg/dkrm/algorithms.js
@@ -1313,16 +1313,8 @@ ex4 : {
 		positions : {
 			position_1 : {
 				state : '1',
-				description : 'Переключатель РАБОТА-НАСТР на ПИУ КД установите в положение НАСТР',
+				description : 'Переведите переключатель РАБОТА-НАСТРОЙКА на ПИУ КД в положение НАСТРОЙКА',
 				action_1 : {
-					inStack : 'stack0',
-					inBlock : 'stack0_block1',
-					onSide : 'stack0_block1_front',
-					type : 'lamp',
-					num : '115',
-					status : 'on',
-				},
-				action_2 : {
 					inStack : 'stack0',
 					inBlock : 'stack0_block1',
 					onSide : 'stack0_block1_front',
@@ -1330,9 +1322,16 @@ ex4 : {
 					num : '100',
 					status : 'off',
 				},
+				action_2 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block1',
+					onSide : 'stack0_block1_front',
+					type : 'lamp',
+					num : '115',
+					status : 'on',
+				},
 			},
 		},
-		defaultPosition : '0',
 	},
 	el2 : {
 		click : '2',
@@ -1346,7 +1345,7 @@ ex4 : {
 		positions : {
 			position_1 : {
 				state : 'on',
-				description : 'Нажмите кнопку ВКЛ ГРМ1',
+				description : 'Включите первый комплект аппаратуры КРМ на излучение, нажимая кнопку ВКЛ КРМ 1 КОМПЛ до включения индикаторов ВКЛ и ИЗЛУЧ КРМ',
 				action_1 : {
 					inStack : 'stack0',
 					inBlock : 'stack0_block1',
@@ -1378,13 +1377,13 @@ ex4 : {
 		positions : {
 			position_1 : {
 				state : 'on',
-				description : 'Нажмите кнопку ПАРАМЕТР. На цифровом индикаторе отобразится номер частотного канала, заданный заводской настройкой.',
+				description : 'Кнопкой ПАРАМЕТР установите ЦСЧ в режим ЧК. На цифровом индикаторе должен высветиться номер частотного канала, установленного при заводской настройке.',
 				action_1 : {
 					inStack : 'stack0',
 					inBlock : 'stack0_block5',
 					onSide : 'stack0_block5_front',
 					type : 'lamp',
-					num : '21',
+					num : '6',
 					status : 'on',
 				},
 				action_2 : {
@@ -1392,8 +1391,8 @@ ex4 : {
 					inBlock : 'stack0_block5',
 					onSide : 'stack0_block5_front',
 					type : 'lamp',
-					num : '10',
-					status : 'on',
+					num : '21',
+					status : 'off',
 				},
 				action_3 : {
 					inStack : 'stack0',
@@ -1401,26 +1400,26 @@ ex4 : {
 					onSide : 'stack0_block5_front',
 					type : 'indicator',
 					num : '9',
-					status : '000',
+					status : '002',
 				},
 			},
 			position_2 : {
 				state : 'on',
-				description : 'Нажмите кнопку ПАРАМЕТР. На цифровом индикаторе отобразится значение мощности в ваттах.',
+				description : 'Кнопкой ПАРАМЕТР установите ЦСЧ в режим ЧК. На цифровом индикаторе должен высветиться номер частотного канала, установленного при заводской настройке.',
 				action_1 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block5',
+					onSide : 'stack0_block5_front',
+					type : 'lamp',
+					num : '6',
+					status : 'off',
+				},
+				action_2 : {
 					inStack : 'stack0',
 					inBlock : 'stack0_block5',
 					onSide : 'stack0_block5_front',
 					type : 'lamp',
 					num : '10',
-					status : 'off',
-				},
-				action_2 : {
-					inStack : 'stack0',
-					inBlock : 'stack0_block5',
-					onSide : 'stack0_block5_front',
-					type : 'lamp',
-					num : '6',
 					status : 'on',
 				},
 				action_3 : {
@@ -1429,63 +1428,7 @@ ex4 : {
 					onSide : 'stack0_block5_front',
 					type : 'indicator',
 					num : '9',
-					status : '000',
-				},
-			},
-			position_3 : {
-				state : 'on',
-				description : 'Нажмите кнопку ПАРАМЕТР. На цифровом индикаторе отобразится коэффициент стоячей волны.',
-				action_1 : {
-					inStack : 'stack0',
-					inBlock : 'stack0_block5',
-					onSide : 'stack0_block5_front',
-					type : 'lamp',
-					num : '6',
-					status : 'off',
-				},
-				action_2 : {
-					inStack : 'stack0',
-					inBlock : 'stack0_block5',
-					onSide : 'stack0_block5_front',
-					type : 'lamp',
-					num : '15',
-					status : 'on',
-				},
-				action_3 : {
-					inStack : 'stack0',
-					inBlock : 'stack0_block5',
-					onSide : 'stack0_block5_front',
-					type : 'indicator',
-					num : '9',
-					status : '000',
-				},
-			},
-			position_4 : {
-				state : 'on',
-				description : 'Нажмите кнопку ПАРАМЕТР. На цифровом индикаторе отобразится температура.',
-				action_1 : {
-					inStack : 'stack0',
-					inBlock : 'stack0_block5',
-					onSide : 'stack0_block5_front',
-					type : 'lamp',
-					num : '15',
-					status : 'off',
-				},
-				action_2 : {
-					inStack : 'stack0',
-					inBlock : 'stack0_block5',
-					onSide : 'stack0_block5_front',
-					type : 'lamp',
-					num : '21',
-					status : 'on',
-				},
-				action_3 : {
-					inStack : 'stack0',
-					inBlock : 'stack0_block5',
-					onSide : 'stack0_block5_front',
-					type : 'indicator',
-					num : '9',
-					status : '000',
+					status : '001',
 				},
 			},
 		},
@@ -1496,24 +1439,495 @@ ex4 : {
 			inStack : 'stack0',
 			inBlock : 'stack0_block14',
 			onSide : 'stack0_block14_front',
+			num : '19',
+			type : 'toggler',
+		},
+		positions : {
+			position_1 : {
+				state : '1',
+				description : 'Проверьте работу ИКР от БМК ИСП-75, для чего переключатель РАБОТА-КАЛИБРОВКА на передней панели ИКР установите в положение КАЛИБРОВКА.',
+			},
+		},
+		defaultPosition : '0',
+	},
+	el5 : {
+		click : '5',
+		current : {
+			inStack : 'stack0',
+			inBlock : 'stack0_block14',
+			onSide : 'stack0_block14_front',
+			num : '36',
+			type : 'socket',
+		},
+		positions : {
+			position_1 : {
+				state : 'on',
+				description : 'Соедините гнездо 12,5 Гц на передней панели ИКР с гнездом 12,5 Гц на передней панели БМК.',
+			},
+		},
+	},
+	el6 : {
+		click : '6',
+		current : {
+			inStack : 'stack1',
+			inBlock : 'stack1_block3',
+			onSide : 'stack1_block3_front',
+			num : '11',
+			type : 'socket',
+		},
+		positions : {
+			position_1 : {
+				state : 'on',
+				description : 'Соедините гнездо 12,5 Гц на передней панели ИКР с гнездом 12,5 Гц на передней панели БМК.',
+			},
+		},
+	},
+	el7 : {
+		click : '7',
+		current : {
+			inStack : 'stack1',
+			inBlock : 'stack1_block3',
+			onSide : 'stack1_block3_front',
+			num : '5',
+			type : 'socket',
+		},
+		positions : {
+			position_1 : {
+				state : 'on',
+				description : 'Соедините гнездо ВЫХОД на передней панели ИКР с гнездом ВЫХОД на передней панели БМК.',
+			},
+		},
+	},
+	el8 : {
+		click : '8',
+		current : {
+			inStack : 'stack0',
+			inBlock : 'stack0_block14',
+			onSide : 'stack0_block14_front',
+			num : '11',
+			type : 'socket',
+		},
+		positions : {
+			position_1 : {
+				state : 'on',
+				description : 'Соедините гнездо 0 на передней панели ИКР с гнездом ВЫХОД на передней панели БМК.',
+			},
+		},
+	},
+	el9 : {
+		click : '9',
+		current : {
+			inStack : 'stack0',
+			inBlock : 'stack0_block14',
+			onSide : 'stack0_block14_front',
+			num : '23',
+			type : 'socket',
+		},
+		positions : {
+			position_1 : {
+				state : 'on',
+				description : 'Соедините гнездо S на передней панели ИКР с гнездом ВЫХОД на передней панели БМК.',
+				action_1 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block14',
+					onSide : 'stack0_block14_front',
+					type : 'lamp',
+					num : '3',
+					status : 'on',
+				},
+				action_2 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block14',
+					onSide : 'stack0_block14_front',
+					type : 'lamp',
+					num : '15',
+					status : 'on',
+				},
+				action_3 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block14',
+					onSide : 'stack0_block14_front',
+					type : 'indicator',
+					num : '15',
+					status : '000.0',
+				},
+				action_4 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block14',
+					onSide : 'stack0_block14_front',
+					type : 'indicator',
+					num : '31',
+					status : '000.0',
+				},
+			},
+		},
+	},
+el10 : {
+		click : '10',
+		current : {
+			inStack : 'stack1',
+			inBlock : 'stack1_block3',
+			onSide : 'stack1_block3_front',
+			num : '8',
+			type : 'toggler',
+		},
+		positions : {
+			position_1 : {
+				state : '1',
+				description : 'Включите БМК.',
+				action_1 : {
+					inStack : 'stack1',
+					inBlock : 'stack1_block3',
+					onSide : 'stack1_block3_front',
+					type : 'lamp',
+					num : '15',
+					status : 'on',
+				},
+			},
+		},
+	},	
+	el11 : {
+		click : '11',
+		current : {
+			inStack : 'stack1',
+			inBlock : 'stack1_block3',
+			onSide : 'stack1_block3_front',
+			num : '4',
+			type : 'knob',
+		},
+		positions : {
+			position_1 : {
+				state : '1',
+				description : 'Проверьте соответствие показаний ИКР установленным на БМК фиксированным значениям КРС %.',
+				action_1 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block14',
+					onSide : 'stack0_block14_front',
+					type : 'indicator',
+					num : '15',
+					status : '005.0',
+				},
+			},
+			position_2 : {
+				state : '2',
+				description : 'Проверьте соответствие показаний ИКР установленным на БМК фиксированным значениям КРС %.',
+				action_1 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block14',
+					onSide : 'stack0_block14_front',
+					type : 'indicator',
+					num : '15',
+					status : '016.5',
+				},
+			},
+			position_3 : {
+				state : '3',
+				description : 'Проверьте соответствие показаний ИКР установленным на БМК фиксированным значениям КРС %.',
+				action_1 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block14',
+					onSide : 'stack0_block14_front',
+					type : 'indicator',
+					num : '15',
+					status : '033.3',
+				},
+			},
+			position_4 : {
+				state : '4',
+				description : 'Проверьте соответствие показаний ИКР установленным на БМК фиксированным значениям КРС %.',
+				action_1 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block14',
+					onSide : 'stack0_block14_front',
+					type : 'indicator',
+					num : '15',
+					status : '050.0',
+				},
+			},
+			position_5 : {
+				state : '5',
+				description : 'Проверьте соответствие показаний ИКР установленным на БМК фиксированным значениям КРС %.',
+				action_1 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block14',
+					onSide : 'stack0_block14_front',
+					type : 'indicator',
+					num : '15',
+					status : '100',
+				},
+			},
+		},
+	},
+	el12 : {
+		click : '12',
+		current : {
+			inStack : 'stack0',
+			inBlock : 'stack0_block14',
+			onSide : 'stack0_block14_front',
+			num : '19',
+			type : 'toggler',
+		},
+		positions : {
+			position_1 : {
+				state : '0',
+				description : 'Переключатель РАБОТА-КАЛИБРОВКА на передней панели ИКР установите в положение РАБОТА ',
+			},
+		},
+		defaultPosition : '1'
+	},
+	el13 : {
+		click : '13',
+		current : {
+			inStack : 'stack0',
+			inBlock : 'stack0_block1',
+			onSide : 'stack0_block1_front',
+			num : '99',
+			type : 'button',
+		},
+		positions : {
+			position_1 : {
+				state : 'off',
+				description : 'Включите на излучение первый комплект аппаратуры',
+			},
+		},
+	},
+	el14 : {
+		click : '14',
+		current : {
+			inStack : 'stack0',
+			inBlock : 'stack0_block14',
+			onSide : 'stack0_block14_front',
 			num : '9',
 			type : 'toggler',
 		},
 		positions : {
 			position_1 : {
 				state : '1',
-				description : 'Установите переключатель КРС-УРОВЕНЬ в положение УРОВЕНЬ',
-			},
-			position_2 : {
-				state : '0',
-				description : 'Установите переключатель КРС-УРОВЕНЬ в положение КРС, снимите показание КРС по цифровым индикаторам по каналам контроля «0» и «S».',
+				description : 'Установите на блоках ИКР переключатели КРС-УРОВЕНЬ ВХОДА в положение УРОВЕНЬ ВХОДА.',
+				action_1 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block14',
+					onSide : 'stack0_block14_front',
+					type : 'indicator',
+					num : '31',
+					status : '50',
+				},
 			},
 		},
 		defaultPosition : '0',
-	},	
+	},
+	el15 : {
+		click : '15',
+		current : {
+			inStack : 'stack0',
+			inBlock : 'stack0_block14',
+			onSide : 'stack0_block14_front',
+			num : '9',
+			type : 'toggler',
+		},
+		positions : {
+			position_1 : {
+				state : '0',
+				description : 'Установите тумблер КРС-УРОВЕНЬ ВХОДА на ИКР в положение КРС и снимите показание КРС по цифровому индикатору по каналам контроля.',
+			},
+		},
+	},
 },
+
 ex5 : {
-	nameOfExercise : '[UNDER_CONSTRUCTION]Установка и контроль положения зоны курса, установка порогов срабатывания выдачи сигналов АВАРИЯ',	
+	nameOfExercise : 'Установка и контроль положения зоны курса, установка порогов срабатывания выдачи сигналов аварии устройств допускового контроля, проверка аварийного переключения комплектов.',
+	el1 : {
+		click : '1',
+		current : {
+			inStack : 'stack0',
+			inBlock : 'stack0_block1',
+			onSide : 'stack0_block1_front',
+			num : '99',
+			type : 'button',
+		},
+		positions : {
+			position_1 : {
+				state : 'on',
+				description : 'Включите первый комплект КРМ на излучение.',
+				action_1 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block1',
+					onSide : 'stack0_block1_front',
+					type : 'lamp',
+					num : '264',
+					status : 'on',
+				},
+				action_2 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block1',
+					onSide : 'stack0_block1_front',
+					type : 'lamp',
+					num : '291',
+					status : 'on',
+				},
+			},
+		},
+	},
+	el2 : {
+		click : '2',
+		current : {
+			inStack : 'stack0',
+			inBlock : 'stack0_block2',
+			onSide : 'stack0_block2_front',
+			num : '1',
+			type : 'rotator',
+		},
+		positions : {
+			position_1 : {
+				state : '5',
+				description : 'Вращением ручки КРУТИЗНА ЛФТ первого комплекта, установите индекс делителя мощности на отметку 50 делений',
+			},
+		},
+	},
+	el3 : {
+		click : '3',
+		current : {
+			inStack : 'stack0',
+			inBlock : 'stack0_block2',
+			onSide : 'stack0_block2_front',
+			num : '2',
+			type : 'rotator',
+		},
+		positions : {
+			position_1 : {
+				state : '8',
+				description : 'Ручкой ФАЗА первого комплекта установите положение ползунка, при котором КРС-ЛЕВО имеет максимальное значение (80)',
+				action_1 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block15',
+					onSide : 'stack0_block15_front',
+					type : 'indicator',
+					num : '31',
+					status : '-40',
+				},
+			},
+		},
+	},
+	el4 : {
+		click : '4',
+		current : {
+			inStack : 'stack0',
+			inBlock : 'stack0_block1',
+			onSide : 'stack0_block1_front',
+			num : '169',
+			type : 'button',
+		},
+		positions : {
+			position_1 : {
+				state : 'on',
+				description : 'Включите второй комплект КРМ на излучение',
+				action_1 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block1',
+					onSide : 'stack0_block1_front',
+					type : 'lamp',
+					num : '636',
+					status : 'on',
+				},
+				action_2 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block1',
+					onSide : 'stack0_block1_front',
+					type : 'lamp',
+					num : '676',
+					status : 'on',
+				},
+			},
+		},
+	},
+	el4 : {
+		click : '4',
+		current : {
+			inStack : 'stack0',
+			inBlock : 'stack0_block3',
+			onSide : 'stack0_block3_front',
+			num : '4',
+			type : 'rotator',
+		},
+		positions : {
+			position_1 : {
+				state : '5',
+				description : 'Ручкой КРУТИЗНА второго комплекта установите ползунок в положение 50 делений',
+			},
+		},
+	},
+	el5 : {
+		click : '5',
+		current : {
+			inStack : 'stack0',
+			inBlock : 'stack0_block3',
+			onSide : 'stack0_block3_front',
+			num : '3',
+			type : 'rotator',
+		},
+		positions : {
+			position_1 : {
+				state : '2',
+				description : 'Ручкой ФАЗА второго комплекта установите ползунок в положение максимального значения КРС-ПРАВО (80)',
+				action_1 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block15',
+					onSide : 'stack0_block15_front',
+					type : 'indicator',
+					num : '31',
+					status : '40',
+				},
+			},
+		},
+	},
+	el6 : {
+		click : '6',
+		current : {
+			inStack : 'stack0',
+			inBlock : 'stack0_block2',
+			onSide : 'stack0_block2_front',
+			num : '2',
+			type : 'rotator',
+		},
+		positions : {
+			position_1 : {
+				state : '6',
+				description : 'Ручкой ФАЗА первого комплекта установите положение ползунка, при котором КРС-ЛЕВО имеет значение -30% (60)',
+				action_1 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block15',
+					onSide : 'stack0_block15_front',
+					type : 'indicator',
+					num : '31',
+					status : '-40',
+				},
+			},
+		},
+	},
+	el7 : {
+		click : '7',
+		current : {
+			inStack : 'stack0',
+			inBlock : 'stack0_block3',
+			onSide : 'stack0_block3_front',
+			num : '3',
+			type : 'rotator',
+		},
+		positions : {
+			position_1 : {
+				state : '4',
+				description : 'Ручкой ФАЗА второго комплекта установите ползунок в положение, при котором КРС-ПРАВО имеет значение 30% (60)',
+				action_1 : {
+					inStack : 'stack0',
+					inBlock : 'stack0_block15',
+					onSide : 'stack0_block15_front',
+					type : 'indicator',
+					num : '31',
+					status : '40',
+				},
+			},
+		},
+	},
 },
 
 ex6 : {
